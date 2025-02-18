fc945c2c372d723345493762fc7be88b72d69dad
jordiForn <thejfk18@gmail.com>
2025-02-18 16:05:06 +0100

    Commit

--- visibility.js
+++ visibility.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuIcon = document.getElementById("menu-icon");
-  const adminMenu = document.getElementById("admin-menu");
+if (window.location.pathname.includes("index.php")) {
+  document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const adminMenu = document.getElementById("admin-menu");
 
-  if (isLoggedIn) {
-    document.getElementById("logout-link").style.display = "inline-block";
-    if (isAdmin) {
-      document.getElementById("menu-icon").style.display = "inline-block";
-      document.getElementById("contact-icon").style.display = "none";
+    if (isLoggedIn) {
+      document.getElementById("logout-link").style.display = "inline-block";
+      if (isAdmin) {
+        document.getElementById("menu-icon").style.display = "inline-block";
+        document.getElementById("contact-icon").style.display = "none";
+      }
     }
-  }
 
-  if (menuIcon) {
     menuIcon.addEventListener("click", function () {
-      if (
-        adminMenu.style.display === "none" ||
-        adminMenu.style.display === ""
-      ) {
+      if (adminMenu.style.display === "none") {
         adminMenu.style.display = "block";
       } else {
         adminMenu.style.display = "none";
       }
     });
-  }
-});
+  });
+}
+
+if (window.location.pathname.includes("contact.php")) {
+}
